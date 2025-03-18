40364997fab553a92954d560919cb0871c2932cc
Ruan Correia <correia.ruan@gmail.com>
2025-03-17 21:21:09 -0300

    Update TesteListaGenerica.java

--- controle/TesteListaGenerica.java
+++ controle/TesteListaGenerica.java
@@ -3,6 +3,6 @@ package controle;
 public class TesteListaGenerica {
     public static void main(String[] args) {
         ControlaAluno controlaAluno = new ControlaAluno();
-        controlaAluno.exibirMenu();  // Inicia o menu do programa
+        controlaAluno.exibirMenu(); 
     }
 }
